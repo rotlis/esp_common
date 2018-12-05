d91fa62b418d60f648ea6fb2c1672e65eae8cd90
robertsayfullin <robert.sayfullin@gmail.com>
2018-12-06 08:08:59 +1100

    updated alisa ip

--- pcm/start.lua
+++ pcm/start.lua
@@ -8,7 +8,7 @@ gpio.write(2, gpio.LOW)
 mqttClient.setCallback(function(client, topic, message)
     tmr.alarm(0, 100, tmr.ALARM_SINGLE, function()
         gpio.write(2, gpio.HIGH)
-        stream.play(pcm.RATE_16K, "192.168.2.32", "1880", "/audio.u8",
+        stream.play(pcm.RATE_16K, "192.168.1.32", "1880", "/audio.u8",
             function()
                 gpio.write(2, gpio.LOW)
             end)
